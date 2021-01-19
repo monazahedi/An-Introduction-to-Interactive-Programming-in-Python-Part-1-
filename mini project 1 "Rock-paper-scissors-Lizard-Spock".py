aad6cbebf0b8802c835503506a7d150f0c87d50f
Mona Zahedi <76893556+monazahedi@users.noreply.github.com>
2021-01-18 16:41:40 -0800

    Add files via upload

---
+++ mini project 1 "Rock-paper-scissors-Lizard-Spock".py
@@ -0,0 +1,73 @@
+# Rock-paper-scissors-lizard-Spock
+
+
+# The key idea of this program is to equate the strings
+# "rock", "paper", "scissors", "lizard", "Spock" to numbers
+# as follows:
+#
+# 0 - rock
+# 1 - Spock
+# 2 - paper
+# 3 - lizard
+# 4 - scissors
+
+# helper functions
+
+import random
+
+def name_to_number(name):
+    if name == "rock":
+        return 0
+    elif name == "Spock":
+        return 1
+    elif name == "paper":
+        return 2
+    elif name == "lizard":
+        return 3
+    elif name == "scissors":
+        return 4
+    else:
+        return "Error: incorrect input"
+
+
+def number_to_name(number):
+    if number == 0:
+        return "rock"
+    elif number == 1:
+        return "Spock"
+    elif number == 2:
+        return "paper"
+    elif number == 3:
+        return "lizard"
+    elif number == 4:
+        return "scissors"
+    else:
+        print "Error: number is not within range"
+    
+
+def rpsls(player_choice): 
+    print 
+    print "Player chooses " + player_choice
+    player_number = name_to_number(player_choice)
+    comp_number = random.randrange(0, 5)
+    comp_choice = number_to_name(comp_number)
+    print "Computer chooses " + comp_choice
+    dif= player_number - comp_number
+    if (dif <= 2) and (dif > 0 ):
+        print "Player wins!"
+    elif (dif > 2) or (dif < 0):
+        print "Computer wins!"
+    else:
+        print "Player and computer tie!"
+
+    
+# test your code - THESE CALLS MUST BE PRESENT IN YOUR SUBMITTED CODE
+rpsls("rock")
+rpsls("Spock")
+rpsls("paper")
+rpsls("lizard")
+rpsls("scissors")
+
+
+
+
